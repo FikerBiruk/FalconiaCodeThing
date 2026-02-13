667984b7b23307ecb47e9c721c58b53c5af784e2
Fiker Biruk <fikerfuffa@gmail.com>
2026-02-13 10:53:29 -0500

    Fix device path and add time delay in reading

--- tempsense.py
+++ tempsense.py
@@ -1,6 +1,10 @@
-import glob
+import glob, time
 
-device = glob.glob('/sys/b1/w1/devices/28-*')[0] + '/w1_slave'
-raw = open(device).read()
+device = glob.glob('/sys/bus/w1/devices/28-*')[0] + '/w1_slave'
+
+while True:
+with open(device, 'r') as f:
+raw = f.read()
 temp = raw.split('t=')[-1]
-print(float(temp)/1000)
+print(float(temp) / 1000)
+time.sleep(1)
