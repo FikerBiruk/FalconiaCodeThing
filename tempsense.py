807d58bad523fed347923a6848d36c803320bb6c
Fiker Biruk <fikerfuffa@gmail.com>
2026-02-13 11:00:02 -0500

    Improve temperature reading with error handling
    Added error handling for temperature reading.

--- tempsense.py
+++ tempsense.py
@@ -3,8 +3,11 @@ import glob, time
 device = glob.glob('/sys/bus/w1/devices/28-*')[0] + '/w1_slave'
 
 while True:
-with open(device, 'r') as f:
-raw = f.read()
-temp = raw.split('t=')[-1]
-print(float(temp) / 1000)
-time.sleep(1)
+  with open(device, 'r') as f:
+    raw = f.read()
+  if "t=" in raw:
+    temp = raw.split("t=")[-1]
+    print(float(temp) / 1000)
+  else:
+    print("smth wrong happened wait a bit")
+  time.sleep(1)
